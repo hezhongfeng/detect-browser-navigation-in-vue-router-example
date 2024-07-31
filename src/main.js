8b128def939d41d51e4ac85320c09a609935877e
hezhongfeng <287645587@qq.com>
2024-07-31 15:43:38 +0800

    ✨ feat(升级插件):

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import DetectBrowserNavigationInVueRouter from 'detect-browser-navigation-in-vue-router'
+// import DetectBrowserNavigationInVueRouter from 'detect-browser-navigation-in-vue-router'
+import DetectBrowserNavigationInVueRouter from '../../detect-browser-navigation-in-vue-router/dist/detect-browser-navigation-in-vue-router.es'
 
 const app = createApp(App)
 
